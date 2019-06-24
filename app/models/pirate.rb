caf419c45f822ac20a2545d14cb4c66ef4f23a6f
Scott <galejscott@gmail.com>
2019-06-24 19:38:14 +0100

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_reader :name, :weight, :height
+  @@pirates = []
+
+  def initialize(arg)
+    @name = arg[:name]
+    @weight = arg[:weight]
+    @height = arg[:height]
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_reader :name, :type, :booty
+  @@ships = []
+
+  def initialize(arg)
+    @name = arg[:name]
+    @type = arg[:type]
+    @booty = arg[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
+  
 end
